9614d0a804d5484db9b22dd71dd040773b9040d9
ahavrius <gastad601@gmail.com>
2019-03-25 19:46:19 +0200

    curve_model

---
+++ GF.py
@@ -0,0 +1,91 @@
+# https://stackoverflow.com/questions/48065360/interpolate-polynomial-over-a-finite-field/48067397#48067397
+
+import itertools
+from sympy.polys.domains import ZZ
+from sympy.polys.galoistools import (gf_irreducible_p, gf_add, \
+                                     gf_sub, gf_mul, gf_rem, gf_gcdex)
+from sympy.ntheory.primetest import isprime
+
+
+class GF():
+    def __init__(self, p, n=1):
+        p, n = int(p), int(n)
+        if not isprime(p):
+            raise ValueError("p must be a prime number, not %s" % p)
+        if n <= 0:
+            raise ValueError("n must be a positive integer, not %s" % n)
+        self.p = p
+        self.n = n
+        if n == 1:
+            self.reducing = [1, 0]
+        else:
+            for c in itertools.product(range(p), repeat=n):
+                poly = (1, *c)
+                if gf_irreducible_p(poly, p, ZZ):
+                    self.reducing = poly
+                    break
+
+    def add(self, x, y):
+        return gf_add(x, y, self.p, ZZ)
+
+    def sub(self, x, y):
+        return gf_sub(x, y, self.p, ZZ)
+
+    def mul(self, x, y):
+        return gf_rem(gf_mul(x, y, self.p, ZZ), self.reducing, self.p, ZZ)
+
+    def inv(self, x):
+        s, t, h = gf_gcdex(x, self.reducing, self.p, ZZ)
+        return s
+
+    def eval_poly(self, poly, point):
+        val = []
+        for c in poly:
+            val = self.mul(val, point)
+            val = self.add(val, c)
+        return val
+
+
+# Class PolyRing, polynomials over a field
+class PolyRing():
+    def __init__(self, field):
+        self.K = field
+
+    def add(self, p, q):
+        s = [self.K.add(x, y) for x, y in \
+             itertools.zip_longest(p[::-1], q[::-1], fillvalue=[])]
+        return s[::-1]
+
+    def sub(self, p, q):
+        s = [self.K.sub(x, y) for x, y in \
+             itertools.zip_longest(p[::-1], q[::-1], fillvalue=[])]
+        return s[::-1]
+
+    def mul(self, p, q):
+        if len(p) < len(q):
+            p, q = q, p
+        s = [[]]
+        for j, c in enumerate(q):
+            s = self.add(s, [self.K.mul(b, c) for b in p] + \
+                         [[]] * (len(q) - j - 1))
+        return s
+
+# to calculate invert elem of x modulo p - prime
+def mod_inv(x, p):
+    return pow(x, p - 2, p)
+
+# apply Polynomial(defined by list of coeffs) to x
+#The coefficients must be in ascending order (``x**0`` to ``x**n``).
+def poly_calc(x, coeffs):
+    if not coeffs :
+        return 0*x
+    n = len(coeffs)
+    y = 0
+    for i in range(0, n):
+        y += coeffs[i]*x**i
+    return y
+
+#polynom derivation
+#The coefficients must be in ascending order (``x**0`` to ``x**n``).
+def deriv_poly(poly):
+    return [poly[i] * i for i in range(1, len(poly))]
---
+++ HEC.py
@@ -0,0 +1,69 @@
+from sympy.polys.domains import ZZ
+from sympy.polys.galoistools import gf_normal, gf_degree
+from GF import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+gf_normal([5, 10, 21, 5, -3], 5, ZZ)
+
+
+class HEC_general():
+    field: GF
+    h: list
+    f: list
+    genus: int
+
+    def __init__(self, p, n, h, f):
+        self.field = GF(p, n)
+        self.h_canonic(h)
+        self.f_canonic(h, f)
+        self.genus = gf_degree(f)/2 - 1
+
+    def h_canonic(self, h):
+        if (self.field.p == 2):
+            self.h = self.field.mul([1], h)
+        else:
+            self.h = []
+
+    def f_canonic(self, h, f):
+        if (self.field.p != 2):
+            h_inv = [ i * mod_inv(4, self.field.p) for i in h]
+            f = self.field.add(f, self.field.mul(h, h_inv))
+        self.f = self.field.mul([1], f)
+
+    def is_canonic(self):
+        return self.h == []
+
+    def is_correct(self):
+        u = np.arange(self.field.p)
+        v = np.arange(self.field.p)
+        h_reverse = self.h[::-1]
+        f_reverse = self.f[::-1]
+        h_deriv = deriv_poly(h_reverse)
+        f_deriv = deriv_poly(f_reverse)
+        res1 = pow(v, 2) + np.multiply(v, poly_calc(u, h_reverse)) - poly_calc(u, f_reverse)
+        res2 = 2 * v + poly_calc(u, h_reverse)
+        res3 = np.multiply(v, poly_calc(u, h_deriv)) - poly_calc(u, f_deriv)
+        return sum((res1 == 0) & (res2 == 0) & (res3 == 0)) == 0
+
+    def drow_curve(self):
+        y, x = np.ogrid[-10:10:100j, -10:10:100j]
+        plt.contour(x.ravel(), y.ravel(), pow(y, 2) + y * poly_calc(x, self.h[::-1]) - poly_calc(x, self.f[::-1]), [0])
+        plt.grid()
+        plt.show()
+
+#    @classmethod
+
+"""
+#Example:
+ff = GF(3, 4)
+g = ff.mul([1], [1, 1, 1])
+print(ff.reducing)
+print(g)
+
+crv = HEC_general(5, 12, [], [1, 0, 0])
+print(crv.h)
+print(crv.f)
+crv.drow_curve()
+print(crv.is_correct())
+"""
